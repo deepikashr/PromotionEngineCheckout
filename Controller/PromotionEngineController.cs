955405d604579b375ac38868c0e6fbef658a8ea6
deepikashr <deepikashr@gmail.com>
2021-06-30 16:30:03 +0530

    Add files via upload

---
+++ Controller/PromotionEngineController.cs
@@ -0,0 +1,44 @@
+﻿using PromotionEngineCheckout.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngineCheckout.Controller {
+    public class EngineController {
+        private IEnumerable<Price> PriceList;
+        private IEnumerable<Promotion> Promotions;
+
+        public EngineController(IEnumerable<Price> priceList, IEnumerable<Promotion> promotions) {
+            this.PriceList = priceList;
+            this.Promotions = promotions;
+        }
+
+        public void CalculatePrice(Order order) {
+            var foundItems = new List<Item>();
+            if (Promotions != null && Promotions.Count() > 0)
+                foreach (var promotion in Promotions) {
+                    var validatedItems = promotion.Validate(order, foundItems);
+                    UpdateValidatedItems(foundItems, validatedItems);
+                }
+
+            ApplyUnitPrice(order, foundItems);
+        }
+
+        private void ApplyUnitPrice(Order order, List<Item> foundItems) {
+            foreach (var item in order.order_items) {
+                var validateItem = foundItems.FirstOrDefault(x => x.sku_id == item.sku_id) ?? item;
+                var quantity = validateItem.quantity;
+                if (quantity > 0)
+                    order.total_amount += quantity * PriceList.First(x => x.sku_id == item.sku_id).unit_price;
+            }
+        }
+
+        private static void UpdateValidatedItems(List<Item> foundItems, IEnumerable<Item> validatedItems) {
+            if (validatedItems == null || validatedItems.Count() < 1)
+                return;
+
+            foreach (var item in validatedItems)
+                if (!foundItems.Any(x => x.sku_id == item.sku_id))
+                    foundItems.Add(item);
+        }
+    }
+}
---
+++ Model/PromotionEngineModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngineCheckout.Model {
+    class PromotionEngineModel {
+    }
+    public class Item {
+        public Item() { }
+        public Item(Item item) {
+            sku_id = this.sku_id;
+            quantity = this.quantity;
+        }
+        public char sku_id { get; set; }
+        public int quantity { get; set; }
+    }
+    public class Price {
+        public char sku_id { get; set; }
+        public int unit_price { get; set; }
+    }
+    public class Order {
+        public List<Item> order_items { get; set; }
+        public double total_amount { get; set; }
+    }
+    public class Promotion : Order {
+        public IEnumerable<Item> Validate(Order order, IEnumerable<Item> validatedItems) {
+            var foundItems = new List<Item>();
+            if (this.order_items == null || this.order_items.Count < 1)
+                return foundItems; //not in order
+
+            foreach (var promotionItem in order_items) {
+                var foundItem = validatedItems.FirstOrDefault(x => x.sku_id == promotionItem.sku_id) ??
+                  order.order_items.FirstOrDefault(x => x.sku_id == promotionItem.sku_id);
+                if (foundItem == null || foundItem.quantity < promotionItem.quantity)
+                    return null;
+
+                if (!foundItems.Any(x => x.sku_id == foundItem.sku_id))
+                    foundItems.Add(foundItem);
+            }
+
+            ApplyPromotionPriceAndCalculateRestQuantity(order, foundItems);
+
+            return foundItems;
+        }
+
+        private void ApplyPromotionPriceAndCalculateRestQuantity(Order order, List<Item> foundItems) {
+            var found = foundItems.Count() > 0;
+            if (found) {
+                do {
+                    order.total_amount += total_amount;
+                    foreach (var promotionItem in order_items) {
+                        var item = foundItems.FirstOrDefault(x => x.sku_id == promotionItem.sku_id);
+                        if (item != null) {
+                            item.quantity -= promotionItem.quantity;
+                            if (found)
+                                found = item.quantity >= promotionItem.quantity;
+                        }
+                    }
+                } while (found);
+            }
+        }
+    }
+}
